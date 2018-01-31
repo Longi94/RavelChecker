34df8cc5f001008dd374362a5a9f6d990c813f89
Long Tran <lngtrn94@gmail.com>
2018-01-31 17:57:01 +0100

    check the websites, send notifications

--- server/index.js
+++ server/index.js
@@ -1,7 +1,19 @@
 const schedule = require('node-schedule');
 const request = require('request');
+const cheerio = require('cheerio');
+const express = require('express');
 
 const key = process.argv[2];
+
+const notifs = []
+
+const sites = [
+    {name: 'ground floor', url: 'http://ravelresidence.studentexperience.nl/plattegrond.php?pagina=2&begane-grond'},
+    {name: 'first floor', url: 'http://ravelresidence.studentexperience.nl/plattegrond.php?pagina=2&eerste-verdieping'},
+    {name: 'second floor', url: 'http://ravelresidence.studentexperience.nl/plattegrond.php?pagina=2&tweede-verdieping'},
+    {name: 'third floor', url: 'http://ravelresidence.studentexperience.nl/plattegrond.php?pagina=2&derde-verdieping'},
+    {name: 'fourth floor', url: 'http://ravelresidence.studentexperience.nl/plattegrond.php?pagina=2&vierde-verdieping'},
+];
 
 const options = {
   url: 'https://fcm.googleapis.com/fcm/send',
@@ -11,8 +23,11 @@ const options = {
   }
 };
 
-const j = schedule.scheduleJob('*/5 * * * * *', function () {
-    sendNotification("hi");
+const j = schedule.scheduleJob('*/5 * * * *', function () {
+    console.log(new Date() + ' checking...');
+    for (let i = 0; i < sites.length; i++) {
+        checkFloor(sites[i]);
+    }
 });
 
 function sendNotification(text) {
@@ -35,3 +50,63 @@ function sendNotification(text) {
         console.log(body);
     });
 }
+
+function checkFloor(floor) {
+    request.get(floor.url, (error, response, body) => {
+        if (error) {
+            console.log(error);
+            return;
+        }
+
+        let $ = cheerio.load(body)
+
+        let $floor = $(
+            '#bovenste-rij,' +
+            '#far-right,' +
+            '#mid-right,' +
+            '#top-right,' +
+            '#bottom-mid-right,' +
+            '#bottom-right,' +
+            '#bottom-left,' +
+            '#bottom-mid-left,' +
+            '#far-left,' +
+            '#mid-left,' +
+            '#top-mid-left,' +
+            '#far-top-left,' +
+            '#mid-top-left-small,' +
+            '#far-top-left-small'
+        )
+
+        const furnished = $floor.find('a.furnature');
+        const unfurnished = $floor.find('a.beschikbaar');
+
+        if (furnished.length > 0) {
+            sendNotification("There are " + furnished.length + " free furnished rooms one the "
+                + floor.name);
+
+            notifs.push({
+                text: furnished.length + " free furnished rooms one the " + floor.name,
+                date: new Date()
+            });
+        }
+
+        if (unfurnished.length > 0) {
+            sendNotification("There are " + unfurnished.length + " free unfurnished rooms one the "
+                + floor.name);
+
+            notifs.push({
+                text: unfurnished.length + " free unfurnished rooms one the " + floor.name,
+                date: new Date()
+            });
+        }
+    });
+}
+
+const app = express();
+
+app.get('/ravel', function (req, res) {
+  res.type('json');
+  res.send(notifs);
+});
+
+app.listen(4444)
