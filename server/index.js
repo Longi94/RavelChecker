7238af4aa9b1ae15470d93301be18e250a5d69b4
Long Tran <lngtrn94@gmail.com>
2018-01-31 19:46:58 +0100

    send proper data

--- server/index.js
+++ server/index.js
@@ -10,11 +10,11 @@ console.log("key=" + key);
 let lastNotification = {};
 
 const sites = [
-    {name: 'ground floor', url: 'http://ravelresidence.studentexperience.nl/plattegrond.php?pagina=2&begane-grond'},
-    {name: 'first floor', url: 'http://ravelresidence.studentexperience.nl/plattegrond.php?pagina=2&eerste-verdieping'},
-    {name: 'second floor', url: 'http://ravelresidence.studentexperience.nl/plattegrond.php?pagina=2&tweede-verdieping'},
-    {name: 'third floor', url: 'http://ravelresidence.studentexperience.nl/plattegrond.php?pagina=2&derde-verdieping'},
-    {name: 'fourth floor', url: 'http://ravelresidence.studentexperience.nl/plattegrond.php?pagina=2&vierde-verdieping'},
+    'http://ravelresidence.studentexperience.nl/plattegrond.php?pagina=2&begane-grond',
+    'http://ravelresidence.studentexperience.nl/plattegrond.php?pagina=2&eerste-verdieping',
+    'http://ravelresidence.studentexperience.nl/plattegrond.php?pagina=2&tweede-verdieping',
+    'http://ravelresidence.studentexperience.nl/plattegrond.php?pagina=2&derde-verdieping',
+    'http://ravelresidence.studentexperience.nl/plattegrond.php?pagina=2&vierde-verdieping'
 ];
 
 const options = {
@@ -27,20 +27,76 @@ const options = {
 
 schedule.scheduleJob('*/1 * * * *', function () {
     console.log(new Date() + ' checking...');
-    for (let i = 0; i < sites.length; i++) {
-        checkFloor(sites[i]);
-    }
+
+    const promises = sites.map(url => new Promise((resolve, reject) => {
+        request.get(url, (error, response, body) => {
+            if (error) {
+                console.log(error);
+                return reject(error);
+            }
+
+            resolve(body);
+        });
+    }));
+
+    Promise.all(promises).then(values => {
+
+        let floors = [];
+        let furnishedRooms = 0;
+        let unfurnishedRooms = 0;
+
+        for (let i = 0; i < values.length; i++) {
+            let $ = cheerio.load(values[i]);
+
+            let $floor = $(
+                '#bovenste-rij,' +
+                '#far-right,' +
+                '#mid-right,' +
+                '#top-right,' +
+                '#bottom-mid-right,' +
+                '#bottom-right,' +
+                '#bottom-left,' +
+                '#bottom-mid-left,' +
+                '#far-left,' +
+                '#mid-left,' +
+                '#top-mid-left,' +
+                '#far-top-left,' +
+                '#mid-top-left-small,' +
+                '#far-top-left-small'
+            );
+
+            const furnished = $floor.find('a.furnature');
+            const unfurnished = $floor.find('a.option');
+
+            if (furnished.length > 0) {
+                floors.push(i);
+                furnishedRooms += furnished.length;
+            }
+
+            if (unfurnished.length > 0) {
+                floors.push(i);
+                unfurnishedRooms += unfurnished.length;
+            }
+        }
+
+        if (furnishedRooms + unfurnishedRooms > 0) {
+            lastNotification = {
+                furnished: furnishedRooms,
+                unfurnished: unfurnishedRooms,
+                timestamp: Date.now()
+            };
+
+            sendMessage(lastNotification);
+        }
+    });
 });
 
-function sendNotification(text) {
+function sendMessage(data) {
     console.log('sending message...');
     const message = {
         to: '/topics/ravel',
         priority: 'high',
-        notification: {
-            body: text,
-            title: 'Ravel Checker'
-        }
+        data: data
     };
 
     let opt = Object.assign(options, {body: JSON.stringify(message)});
@@ -53,56 +109,7 @@ function sendNotification(text) {
     });
 }
 
-function checkFloor(floor) {
-    request.get(floor.url, (error, response, body) => {
-        if (error) {
-            console.log(error);
-            return;
-        }
-
-        let $ = cheerio.load(body);
-
-        let $floor = $(
-            '#bovenste-rij,' +
-            '#far-right,' +
-            '#mid-right,' +
-            '#top-right,' +
-            '#bottom-mid-right,' +
-            '#bottom-right,' +
-            '#bottom-left,' +
-            '#bottom-mid-left,' +
-            '#far-left,' +
-            '#mid-left,' +
-            '#top-mid-left,' +
-            '#far-top-left,' +
-            '#mid-top-left-small,' +
-            '#far-top-left-small'
-        );
-
-        const furnished = $floor.find('a.furnature');
-        const unfurnished = $floor.find('a.option');
-        const now = Date.now();
-
-        if (furnished.length > 0) {
-            sendNotification("There are " + furnished.length + " free furnished rooms one the "
-                + floor.name);
-
-            lastNotification = {
-                text: furnished.length + " free furnished rooms on the " + floor.name,
-                date: now
-            };
-        }
-
-        if (unfurnished.length > 0) {
-            sendNotification("There are " + unfurnished.length + " free unfurnished rooms one the "
-                + floor.name);
-
-            lastNotification = {
-                text: unfurnished.length + " free unfurnished rooms on the " + floor.name,
-                date: now
-            };
-        }
-    });
+function checkFloor(url, i) {
 }
 
 const app = express();
